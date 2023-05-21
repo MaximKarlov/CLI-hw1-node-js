71ba0cc0db0f82a070c3313fef1ae66f4efff22e
MaximKarlov <predator09071987@gmail.com>
2023-05-21 11:32:45 +0300

    fix error after audit

--- contacts.js
+++ contacts.js
@@ -2,63 +2,55 @@ const contactsPath = './db/contacts.json';
 const fs = require('fs').promises;
 
 const listContacts = async () => {
-  const list = await fs
-    .readFile(contactsPath, 'utf-8')
-    .then(data => console.table(JSON.parse(data)))
-    .catch(err => console.log(err));
-  return list;
+  try {
+    const list = await fs.readFile(contactsPath, 'utf-8');
+    console.table(JSON.parse(list));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const getContactById = async contactId => {
-  await fs
-    .readFile(contactsPath, 'utf-8')
-    .then(contacts => {
-      return JSON.parse(contacts);
-    })
-    .then(contacts => {
-      const searchContacts = contacts.filter(el => el.id === contactId);
-      if (searchContacts.length > 0) return console.table(searchContacts);
-      else return console.log('No contacts found');
-    })
-    .catch(err => console.log(err));
+  try {
+    const contactsList = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(contactsList);
+    const searchContacts = contacts.filter(el => el.id === contactId);
+    if (searchContacts.length > 0) return console.table(searchContacts);
+    else return console.log('No contacts found');
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const removeContact = async contactId => {
-  let flag = null;
   let searchContacts = [];
-  await fs
-    .readFile(contactsPath, 'utf-8')
-    .then(contacts => {
-      return JSON.parse(contacts);
-    })
-    .then(contacts => {
-      contacts.map(el => {
-        if (el.id === contactId) {
-          return (searchContacts = contacts.filter(el => el.id !== contactId));
-        }
-      });
-      if (searchContacts.length > 0) {
-        fs.writeFile(contactsPath, JSON.stringify(searchContacts));
-        console.log('Contacts delete successfully');
-      } else console.log('No contacts found');
-    })
-    .catch(err => console.log(err));
+  try {
+    const contactsList = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(contactsList);
+    contacts.map(el => {
+      if (el.id === contactId) {
+        return (searchContacts = contacts.filter(el => el.id !== contactId));
+      }
+    });
+    if (searchContacts.length > 0) {
+      fs.writeFile(contactsPath, JSON.stringify(searchContacts));
+      console.log('Contacts delete successfully');
+    } else console.log('No contacts found');
+  } catch (err) {
+    console.log(err);
+  }
   listContacts();
 };
 
 const addContact = async newUser => {
-  const contactsArray = await fs
-    .readFile(contactsPath, 'utf-8')
-    .then(contacts => {
-      return JSON.parse(contacts);
-    })
-    .then(contacts => {
-      contacts.push(newUser);
-      return contacts;
-    })
-    .catch(err => console.log(err));
-  fs.writeFile(contactsPath, JSON.stringify(contactsArray));
-  console.log('Contacts added successfully');
+  try {
+    const contactsList = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(contactsList);
+    contacts.push(newUser);
+    fs.writeFile(contactsPath, JSON.stringify(contacts));
+  } catch (err) {
+    console.log(err);
+  }
   listContacts();
 };
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Command } = require('commander');
+const shortid = require('shortid');
 const list = require('./contacts.js');
 const program = new Command();
 program
@@ -23,6 +24,9 @@ function invokeAction({ action, id, name, email, phone }) {
       break;
 
     case 'add':
+      if (id === undefined) {
+        id = shortid.generate();
+      }
       newUser = {
         id,
         name,
